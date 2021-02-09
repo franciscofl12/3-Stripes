f9bea336404c3bf0acb003d5501658f56244b755
Francisco Flores <54539494+franciscofl12@users.noreply.github.com>
2021-02-09 17:21:26 +0100

    Fix some simple things

--- src/me/franciscofl12/threestripes/Game.java
+++ src/me/franciscofl12/threestripes/Game.java
@@ -1,12 +1,11 @@
 package me.franciscofl12.threestripes;
 
 import java.awt.BorderLayout;
+
 import java.awt.Canvas;
 import java.awt.Graphics2D;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.awt.image.BufferStrategy;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,11 +17,13 @@ public class Game extends Canvas{
 	
 	private static Game instance = null;
 	private static JFrame ventana = null;
-	private static List<Actor> actores = new ArrayList<Actor>();
 	static int[][] tablero = new int[3][3];
 	static final int FPS = 60;
 	static int turno = 1;
 	static int contador = 0;
+	static int posicionx = 0; // Variable para colocar la imagen
+	static int posiciony = 0;
+	private static List<Actor> actores = new ArrayList<Actor>();
 	
 	// Creo un doble buffer que lo utilizare para que no muestre lagazos a la hora de procesar los ladrillos
 	public BufferStrategy strategy;
@@ -41,10 +42,7 @@ public class Game extends Canvas{
 		ventana.setBounds(0, 0, 600, 600);
 		// Para colocar objetos sobre la ventana debo asignarle un "layout" (plantilla) al panel principal de la ventana
 		ventana.getContentPane().setLayout(new BorderLayout());
-		// Creo una lista de actores que intervendrá en el juego.
-		actores = creaTablero();	
 		// Creo y agrego un canvas, es un objeto que permitirá dibujar sobre él
-		
 		ventana.getContentPane().add(this, BorderLayout.CENTER);
 		// Consigo que la ventana no se redibuje por los eventos de Windows
 		ventana.setIgnoreRepaint(true);
@@ -90,61 +88,65 @@ public class Game extends Canvas{
 		}
 	}
 	
+	/****
+	 * Pintamos el fondo del juego
+	 */
 	@SuppressWarnings("static-access")
 	public void pintaMundo() {
 		Graphics2D g = (Graphics2D) strategy.getDrawGraphics();
 		
 		g.drawImage(GameSprite.getInstance().getSprite("tablero.png"),0,0,null); // Fondo
 		
+		for (Actor a : this.actores) {
+			a.paintImagen(g);
+		}
+		
 		strategy.show(); 
 	}
 	
+	/****
+	 * Iniciamos el juego
+	 */
 	public void juego() {
 		do {
 		pintaMundo();
 		} while (true);
 	}
 	
-	public static List<Actor> creaTablero() {
-		
-		
-		return actores;
-	}
-	
-	public static void rellenarTablero(int jugador, int x, int y) {
-		System.out.println(x);
-		System.out.println(y);
+	public void rellenarTablero(int jugador, int x, int y) {
+		//Comprobamos donde clickó el jugador
 		if (x<=200) {
-			x = 0;
+			x = 1;
 		}
 		else {
 			if (x<=400) {
-				x = 1;
+				x = 2;
 			}
 			else {
 				if (x<=600) {
-					x = 2;
+					x = 3;
 				}
 			}
 		}
 		if (y<=200) {
-			y = 0;
+			y = 1;
 		}
 		else {
 			if (y<=400) {
-				y = 1;
+				y = 2;
 			}
 			else {
 				if (y<=600) {
-					y = 2;
+					y = 3;
 				}
 			}
 		}
 		x = x - 1;
 		y = y - 1;
+		//Agregamos la accion del jugador al tablero, o avisamos que no está disponible esa casilla
 		for (int i = 0; i < tablero.length; i++) {
 			for (int j = 0; j < tablero[i].length; j++) {
-				if (x == i && y == j) {
+				if (x == j && y == i) {
 					if (tablero[i][j] == 0) {
 						tablero[i][j] = jugador;
 						if (jugador == 1) {
@@ -152,6 +154,21 @@ public class Game extends Canvas{
 						}
 						if (jugador == 2) {
 							turno = 1;
+						}
+						// Aquí obtendremos donde irán colocadas las imagenes
+						if (x == 0) posicionx = 10; if (x == 1) posicionx = 210; if (x == 2) posicionx = 410; 
+						if (y == 0) posiciony = 10; if (y == 1) posicionx = 210; if (y == 2) posicionx = 410;
+						if (jugador == 1) {
+							Cross equis = new Cross(posicionx,posiciony);
+							equis.x = posicionx;
+							equis.y = posiciony;
+							actores.add(equis);
+						}
+						if (jugador == 2) {
+							Circle circulo = new Circle(posicionx,posiciony);
+							circulo.x = posicionx;
+							circulo.y = posiciony;
+							actores.add(circulo);
 						}
 					}
 					else JOptionPane.showMessageDialog(null,"Jugador " + jugador + " vuelve a elegir el jugador y las coordenadas. Esa posicion esta ya ocupada");
